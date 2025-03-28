fc33bd8719b7bdcd4b8b3412a87484f9cda8e70f
Miggy Basina <basina.mi.bsinfotech@gmail.com>
2025-03-28 21:19:15 +0800

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,73 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
+
+
 
 const app = express();
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Allows Parsing of JSON request bodies
 
-const items = [
-    {id: 1, name: "Item 1"},
-    {id: 2, name: "Item 2"},
-    {id: 3, name: "Item 3"}
-];
+// MYSQL Connection
 
-app.get("/api/items", (req, res) => {
-    res.json(items)
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'subjects'
+});
+
+db.connect(err => {
+  if(err) throw err;
+  console.log("MySQL connected...")
 });
 
 
-app.post("/api/items",(req, res) => {
-    const newItem = {id: items.length + 1, name: req.body.name};
-    items.push(newItem);
-    res.status(201).json(newItem);
+// GET ALL subjects IN THE DATABASE
+
+app.get('/api/subjects', (req, res) =>{
+    db.query("SELECT * FROM subjects", (err, results)=> {
+      if(err) throw err;
+      res.json(results);
+    });
 });
 
-app.put("/api/items/:id", (req, res) => {
-    const item = items.find((i) => i.id === parseInt(req.params.id));
-    if (!item) return res.status(404).json({message: "Item not found"});
 
-    item.name = req.body.name;
-    res.json(item);
+
+// PUT: UPDATE AN ITEM
+
+app.put('/api/subjects/:id', (req, res) =>{
+    const { id } = req.params;
+    const { Subject_Description, Day, Time, Room } = req.body;
+
+    db.query("UPDATE subjects SET Subject_Description = ?, Day = ?, Time = ?, Room = ? WHERE id = ?", [Subject_Description, Day, Time, Room, id], (err) =>{
+      if(err) throw err;
+      res.json({id, Subject_Description, Day, Time, Room});
+    });
 });
 
-app.delete("/api/items/:id", (req, res) => {
-    const index = items.findIndex((i) => i.id === parseInt(req.params.id));
-    items.splice(index, 1);
-    res.json({message: "Item Deleted"});
+
+app.post('/api/subjects', (req, res) =>{
+      const {Subject_Description, Day, Time, Room} = req.body;
+      db.query("INSERT INTO subjects (Subject_Description, Day, Time, Room) VALUES (?, ?, ?, ?)", [Subject_Description, Day, Time, Room], (err, result) =>{
+        if(err) throw err;
+        res.json({ id: result.insertId, Subject_Description, Day, Time, Room})
+      });
 });
+
+
+// DELETE: remove an Item
+
+
+app.delete('/api/subjects/:id', (req, res) =>{
+  const { id } = req.params;
+  db.query("DELETE FROM subjects WHERE id = ?", [id], (err)=>{
+    if(err) throw err;
+    res.json({message: "Item deleted"});
+  });
+});
+
+
 
 
 const PORT = 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,63 +2,102 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 
-const API_URL = "http://localhost:5000/api/items";
+const API_URL = "http://localhost:5000/api/subjects";
 
 function App (){
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
+  const [subjects, setSubjects] = useState([]);
+  const [subjectDescription, setSubjectDescription] = useState("");
+  const [day, setDay] = useState("");
+  const [time, setTime] = useState("");
+  const [room, setRoom] = useState("");
 
   useEffect(() => {
     axios
     .get(API_URL)
-    .then((response) => setItems(response.data))
+    .then((response) => setSubjects(response.data))
     .catch((error) => console.error("Error fetching items:", error));
   }, []);
 
-const addItem = () => {
+const addSubject = () => {
   axios
-  .post(API_URL, {name: newItem})
-  .then((response) => setItems([...items, response.data]))
+  .post(API_URL, {Subject_Description: subjectDescription, Day: day, Time: time, Room: room})
+  .then((response) => {
+    
+    setSubjects([...subjects, response.data])
+    setSubjectDescription("");
+    setDay("");
+    setTime("");
+    setRoom("");
+  
+  })
   .catch((error) => console.error("Error adding items:", error));
 };
 
-const updateItem = (id, name) => {
+const updateSubject = (id, field, value) => {
+
+  const subjectToUpdate = subjects.find(subject => subject.id === id);
+  if(!subjectToUpdate) return;
+
+  const updateData = {
+    Subject_Description: subjectToUpdate.Subject_Description,
+    Day: subjectToUpdate.Day,
+    Time: subjectToUpdate.Time,
+    Room: subjectToUpdate.Room,
+    [field]: value
+  };
+  
   axios
-  .put(`${API_URL}/${id}`, { name })
+  .put(`${API_URL}/${id}`, updateData)
   .then((response) => {
-    setItems(items.map((item) => (item.id === id ? response.data : item)));
+    setSubjects(subjects.map((subject) => (subject.id === id ? response.data : subject)));
   })
-  .catch((error) => console.error("Error updating item:", error));
+  .catch((error) => console.error("Error updating subject:", error));
 };
 
-const deleteItem = (id) => {
+const deleteSubject = (id) => {
   axios
   .delete(`${API_URL}/${id}`)
   .then(() => {
-    setItems(items.filter((item) => item.id !== id));
+    setSubjects(subjects.filter((subject) => subject.id !== id));
   })
-  .catch((error) => console.error("Error deleting item:", error));
+  .catch((error) => console.error("Error deleting subject:", error));
 };
 
 
 return(
-  <div> 
-    <center>
-    <h1>REact + Express REST API</h1>
-    <input type="text" value={newItem} onChange={(e) => setNewItem(e.target.value)} placeholder = "New Item "/>
-    <input type="button" value="Add Item" onClick={addItem}/>
-    <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-              <input type="text" value={item.name} onChange={(e) => updateItem(item.id, e.target.value)} />
-              <input type="button" value="delete" onClick={() => deleteItem(item.id)} />
-        </li>
-      ))}
-    </ul>
+  <div style={{marginLeft:"100px"}}>
+    <center> 
+    <h1>Assignment #1 (Subject List)</h1>
+    <div style={{ display: "flex", marginBottom: "10px" }}>
+        <input style={{width:"500px"}} type="text" value={subjectDescription} onChange={(e) => setSubjectDescription(e.target.value)} placeholder="Subject Description" />
+        <input type="text" value={day} onChange={(e) => setDay(e.target.value)} placeholder="Day" />
+        <input type="text" value={time} onChange={(e) => setTime(e.target.value)} placeholder="Time" />
+        <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} onKeyDown={(e) => {if (e.key === "Enter") {addSubject();}}} placeholder="Room" />
+        <input type="button" value="Add Subject" onClick={addSubject} />
+    </div>
+    <table border="1px">
+        <thead>
+            <tr>
+            <th>Subject</th>
+            <th>Day</th>
+            <th>Time</th>
+            <th>Room</th>
+            </tr>
+        </thead>
+        <tbody>
+            {subjects.map((subject) => (
+                <tr key={subject.id}>
+                    <td><input style={{width:"500px", textAlign:"center"}} type="text" value={subject.Subject_Description} onChange={(e) => updateSubject(subject.id, "Subject_Description", e.target.value)}/></td>
+                    <td><input type="text" value={subject.Day} onChange={(e) => updateSubject(subject.id, "Day", e.target.value)} /></td>
+                    <td><input type="text" value={subject.Time} onChange={(e) => updateSubject(subject.id, "Time", e.target.value)} /></td>
+                    <td><input type="text" value={subject.Room} onChange={(e) => updateSubject(subject.id, "Room", e.target.value)} /></td>
+                    <td><input type="button" value="Delete" onClick={() => deleteSubject(subject.id)} /></td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
     </center>
-  </div>
+</div>
 );
-
 };
-
 export default App;
